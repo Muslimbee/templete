7ac8afd1eda45745762a33dffa44f4678a35c39a
Muh1isa <lisaa27.a6@gmail.com>
2024-02-03 15:01:12 +0500

    navigation bar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
+import './NavbarStyle.scss'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
         <nav className='nav'>
             <div className="nav__logo">
-                μTech
+                <Link to='/' className='link'>
+                    μTech
+                </Link>
             </div>
             <ul className='nav__list'>
-                <li className="nav__list-item">Home</li>
-                <li className="nav__list-item">About us</li>
-                <li className="nav__list-item">Servise</li>
-                <li className="nav__list-item">Our projects</li>
-                <li className="nav__list-item">Contact us</li>
+                <li className="nav__list-item">
+                    <Link to='/' className='link'>
+                        Home
+                    </Link>
+                </li>
+                <li className="nav__list-item">
+                    <Link to='about' className='link'>
+                        About us
+                    </Link>
+                </li>
+                <li className="nav__list-item">
+                    <Link to='service' className='link'>
+                        Service
+                    </Link>
+                </li>
+                <li className="nav__list-item">
+                    <Link to='/projects' className='link'>
+                        Our projects
+                    </Link>
+                </li>
+                {/* <li className='nav__list-item'>
+                    <select name="" id="">
+                        <option value="">Uzbek</option>
+                        <option value="">English</option>
+                        <option value="">Russian</option>
+                    </select>
+                </li> */}
+                <li className="nav__list-item">
+                    <button className='nav__list-item-btn'>
+                        <Link to='/contact' className='link'>
+                            Contact us
+                        </Link>
+                    </button>
+                </li>
             </ul>
+            <i className="fa-solid fa-bars"></i>
         </nav>
     )
 }
--- src/pages/about us/AboutUs.jsx
+++ src/pages/about us/AboutUs.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import avatar from '../../../public/images/user-tie-solid.svg' 
 
 function AboutUs() {
   return (
-    <div>
-      
+    <div className='about'>
+      <h2>Lorem ipsum dolor sit.</h2>
+      <ul className="about__list">
+        <li className="about__list-item">
+          <img src={avatar} alt="" width={60} />
+          
+          <h3>Muhammad Allayev</h3>
+          <p>Lorem, ipsum.</p>
+          <div className="icons">
+            <i className="fa-brands fa-linkedin"></i>
+            <i className="fa-brands fa-facebook"></i>
+            <i className="fa-brands fa-telegram"></i>
+          </div>
+        </li>
+        <li className="about__list-item">
+          <img src={avatar} alt="" width={60} />
+          
+          <h3>Muhammad Allayev</h3>
+          <p>Lorem, ipsum.</p>
+          <div className="icons">
+            <i className="fa-brands fa-linkedin"></i>
+            <i className="fa-brands fa-facebook"></i>
+            <i className="fa-brands fa-telegram"></i>
+          </div>
+        </li>
+        <li className="about__list-item">
+          <img src={avatar} alt="" width={60} />
+          
+          <h3>Muhammad Allayev</h3>
+          <p>Lorem, ipsum.</p>
+          <div className="icons">
+            <i className="fa-brands fa-linkedin"></i>
+            <i className="fa-brands fa-facebook"></i>
+            <i className="fa-brands fa-telegram"></i>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
