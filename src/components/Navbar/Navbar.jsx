5413f60000b018f9b04a9e824b954ce23f5c3221
Muh1isa <lisaa27.a6@gmail.com>
2024-02-03 13:46:13 +0500

    ...

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 function Navbar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <nav className='nav'>
+            <div className="nav__logo">
+                μTech
+            </div>
+            <ul className='nav__list'>
+                <li className="nav__list-item">Home</li>
+                <li className="nav__list-item">About us</li>
+                <li className="nav__list-item">Servise</li>
+                <li className="nav__list-item">Our projects</li>
+                <li className="nav__list-item">Contact us</li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
