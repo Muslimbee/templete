fad50329a4dd6331a06d25edfa047c5ae2320182
Muslimbee <shaymardonovmuslim27@gmail.com>
2024-02-03 15:13:08 +0500

    ,

--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,24 +1,81 @@
 import React from 'react'
+import './ServiceStyle.scss'
+import { FaTasks } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa6";
 
 function Service() {
   return (
-    <div>
-      <h1>Our Service</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati nesciunt reprehenderit corporis culpa quos? Error dignissimos sequi iure perspiciatis aperiam esse. Quibusdam amet atque laborum eaque nulla doloribus, temporibus consequatur.</p>
-      <div className="cards">
-        <div className="card">
-          <button>
-            <img src="" alt="" />
-          </button>
-        </div>
-        <div className="card"></div>
-        <div className="card"></div>
-      </div>
-      <div className="cards">
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-      </div>
+    <div className='Service' id='Service'>
+     <div className="containere">
+     <h1>Our Service</h1>
+      <p>We turn information into actionable insights We work to understand your issues
+      and are driven to ask better questions in the pursuit of making work.</p>
+     <ul>
+      <li>
+        <button>
+       <div className="task blueviolet">
+       <FaTasks className='task-icon'/>
+       </div>
+        <h3>Order Management</h3>
+        <span>Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</span>
+        <a href="#">Learn more <FaArrowRight  className='right-icon'/></a>
+        </button>
+      </li>
+      <li>
+      <button>
+       <div className="task red">
+       <FaTasks className='task-icon'/>
+       </div>
+        <h3>Order Management</h3>
+        <span>Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</span>
+        <a href="#">Learn more <FaArrowRight  className='right-icon'/></a>
+        </button>
+      </li>
+      <li>
+      <button>
+       <div className="task blue">
+       <FaTasks className='task-icon'/>
+       </div>
+        <h3>Order Management</h3>
+        <span>Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</span>
+        <a href="#">Learn more <FaArrowRight  className='right-icon'/></a>
+        </button>
+      </li>
+     </ul>
+
+     <ul>
+      <li>
+      <button>
+       <div className="task green">
+       <FaTasks className='task-icon'/>
+       </div>
+        <h3>Order Management</h3>
+        <span>Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</span>
+        <a href="#">Learn more <FaArrowRight  className='right-icon'/></a>
+        </button>
+      </li>
+      <li>
+      <button>
+       <div className="task tamoto">
+       <FaTasks className='task-icon'/>
+       </div>
+        <h3>Order Management</h3>
+        <span>Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</span>
+        <a href="#">Learn more <FaArrowRight  className='right-icon'/></a>
+        </button>
+      </li>
+      <li>
+      <button>
+       <div className="task">
+       <FaTasks className='task-icon'/>
+       </div>
+        <h3>Order Management</h3>
+        <span>Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</span>
+        <a href="#">Learn more <FaArrowRight  className='right-icon'/></a>
+        </button>
+      </li>
+     </ul>
+     </div>
     </div>
   )
 }
