cba4e26520e2945baeaee7914f1e75fa124807c5
Muh1isa <lisaa27.a6@gmail.com>
2024-02-03 13:38:26 +0500

    file structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home'
 import AboutUs from './pages/about us/AboutUs'
 import Service from './pages/Service/Service'
 import ContactUs from './pages/contact us/ContactUs'
+import Projects from './pages/projects/Projects'
 
 function App() {
 
@@ -15,8 +16,8 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<AboutUs />} />
         <Route path='/service' element={<Service />} />
+        <Route path='/projects' element={<Projects />} />
         <Route path='/contact' element={<ContactUs />} />
-
       </Routes>
     </>
   )
